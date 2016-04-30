072b4604303bf1e4a6cc24501bc8e20c9d403433
andres naranjo <afnarqui@hotmail.com>
2016-04-30 03:25:29 -0500

    function

---
+++ app.js
@@ -0,0 +1,15 @@
+function suma(num1,num2){
+    return num1 + num2 
+}
+
+function resta(num1,num2){
+    return num1 - num2 
+}
+
+function multiplicacion(num1,num2){
+    return num1 * num2 
+}
+
+function division(num1,num2){
+    return num2 > 0 ? num1 /num2 : 0 
+}
