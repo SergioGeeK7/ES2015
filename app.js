1d414cffbc2e442e3420388feb3f9c7c1ec9907f
andres naranjo <afnarqui@hotmail.com>
2016-04-30 03:36:07 -0500

    callback asincróno

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 function suma(num1,num2){
     return num1 + num2 
 }
@@ -22,3 +23,43 @@ console.log(calculadora(suma,1,2))
 console.log(calculadora(resta,1,2))
 console.log(calculadora(multiplicacion,1,2))
 console.log(calculadora(division,1,2))
+
+*/
+
+/**
+ * callback asincróno
+ */
+
+var id = 10
+
+obtenerdatos(id,function(item){
+    console.log("el nombre es :" + item.nombre)    
+})
+
+function obtenerdatos(id,fn){
+    setTimeout(function(){
+        fn({nombre:"andres","apellido":"naranjo"})
+    },5000)
+}
+
+console.log('se ejecuta antes')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
