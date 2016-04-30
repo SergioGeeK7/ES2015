6f4914be7fc72172f61fabee68d13d167c7d1247
andres naranjo <afnarqui@hotmail.com>
2016-04-30 03:30:45 -0500

    enviar una function

--- app.js
+++ app.js
@@ -13,3 +13,12 @@ function multiplicacion(num1,num2){
 function division(num1,num2){
     return num2 > 0 ? num1 /num2 : 0 
 }
+
+function calculadora(cuenta,num1,num2){
+    return  cuenta(num1,num2)
+}
+
+console.log(calculadora(suma,1,2))
+console.log(calculadora(resta,1,2))
+console.log(calculadora(multiplicacion,1,2))
+console.log(calculadora(division,1,2))
